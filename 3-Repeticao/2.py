c78dcffe4a2699a04704768ec3db6b3f4fb89005
Aline Freitas <aline.do.freitas@gmail.com>
2015-12-03 11:43:57 -0200

    Updated Lista

--- 3-Repeticao/2.py
+++ 3-Repeticao/2.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 #coding:utf-8
-Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+username = raw_input("Choose a Username: ")
+
+While True:
+  password = raw_input("Enter a Password: ")
+  if username == password:
+    print "Your password cannot be equal to your username"
+    password = raw_input("Enter a new Password: ")
